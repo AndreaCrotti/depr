7f56702b001d15e2187fe1b37e4a6f3251e53b2c
Andrea Crotti <andrea.crotti.0@gmail.com>
2016-10-05 17:21:19 +0100

    rename and move docs

--- depr/__init__.py
+++ depr/__init__.py
@@ -3,6 +3,24 @@ import warnings
 
 
 class Deprecator(object):
+    def __init__(self, reason=None, replacement=None):
+        assert not (reason and replacement), "can only pass msg or replacement, not both"
+        if replacement is not None:
+            assert callable(replacement), "Replacement function needs to be a callable as well"
+            self.message = "Use new function {} instead".format(replacement.__name__)
+        else:
+            self.message = reason or "Call to deprecated function"
+
+    def __call__(self, func):
+        @functools.wraps(func)
+        def _deprecate(*args, **kwargs):
+            warnings.warn(self.message, category=DeprecationWarning, stacklevel=2)
+            return func(*args, **kwargs)
+
+        return _deprecate
+
+
+def deprecate(function=None, *args, **kwargs):
     """Mark a function as deprecated, can be used in these ways
 
     @deprecate(reason="Changed to a better way")
@@ -19,25 +37,7 @@ class Deprecator(object):
         pass
 
     """
-    def __init__(self, reason=None, replacement=None):
-        assert not (reason and replacement), "can only pass msg or replacement, not both"
-        if replacement is not None:
-            assert callable(replacement), "Replacement function needs to be a callable as well"
-            self.reason = "Use new function {} instead".format(replacement.__name__)
-        else:
-            self.reason = reason or "Call to deprecated function"
-
-    def __call__(self, func):
-        @functools.wraps(func)
-        def _deprecate(*args, **kwargs):
-            warnings.warn(self.reason, category=DeprecationWarning, stacklevel=2)
-            return func(*args, **kwargs)
-
-        return _deprecate
-
-
-def deprecate(func_or_reason=None, *args, **kwargs):
-    if callable(func_or_reason):
-        return Deprecator()(func_or_reason)
+    if callable(function):
+        return Deprecator()(function)
     else:
         return Deprecator(*args, **kwargs)
