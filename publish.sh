ef10eb8d98fb11f723edfcfede38c3d9cf18265e
Andrea Crotti <andrea.crotti.0@gmail.com>
2016-10-05 15:15:25 +0100

    fix publishs cript

--- publish.sh
+++ publish.sh
@@ -2,7 +2,7 @@
 
 # TODO: get a version number in input to add a git tag
 
-if $1 == "LIVE"
+if [ "$1" == "LIVE" ]
 then
     python setup.py register -r pypi
     python setup.py sdist upload -r pypi
